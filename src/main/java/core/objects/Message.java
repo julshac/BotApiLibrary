0856b9c1084fdecb4d2707c885b5c1997b3bd90a
Sergey <sergeygerasimov778@gmail.com>
2016-12-06 01:36:36 +0300

    Add method for more simple access to text message

--- src/main/java/core/objects/Message.java
+++ src/main/java/core/objects/Message.java
@@ -1,6 +1,13 @@
 package core.objects;
 
+import java.util.Optional;
+
 public interface Message {
     Chat getChat();
     MessageType getType();
+
+    default Optional<TextMessage> asTextMessage() {
+        return getType() == MessageType.TEXT_MESSAGE ?
+                Optional.of((TextMessage) this) : Optional.empty();
+    }
 }
